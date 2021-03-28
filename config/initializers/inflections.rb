26f11f55bf96d15cd0995ac7bb4d62f6bf19a746
Juan Vasquez Gonzalez <juan@logicalbricks.com>
2021-03-28 15:02:55 -0600

    agrega la configuracion de espaniol en rails

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,111 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections do |inflect|
+
+  # La regla básica en Español es similar al Inglés, el plural de las palabras
+  # terminadas en vocal se obtiene agregándole una 's' al final. Sin embargo,
+  # para formar correctamente el plural de palabras como doctor, camion,
+  # universidad ley o pastel, se aplica una regla distinta: las palabras que
+  # terminen con r, y, n, d, l se convierten a plural agregando la sílaba "es",
+  # de otra forma, el plural se obtendrían como doctors, camions, universidads,
+  # leys y pastels.
+  # Para obtener correctamente el plural de palabras como maiz => maices,
+  # pez => peces, las palabras terminadas en z se cambian la z por c, y
+  # se agrega "es".
+  # Por facilidad, haremos uso de la posibilidad que nos brinda `gsub` de pasar
+  # un hash para realizar los remplazos.
+
+  # remplazos = { a: "as", e: "es", i: "is", o: "os", u: "us", d: "des", j: "jes", l: "les", n: "nes", r: "res", y: "yes", z: "ces", }
+
+  inflect.plural(/(z|[a])(?=[A-Z]|_|$)/, "as")
+  inflect.plural(/(z|[e])(?=[A-Z]|_|$)/, "es")
+  inflect.plural(/(z|[i])(?=[A-Z]|_|$)/, "is")
+  inflect.plural(/(z|[o])(?=[A-Z]|_|$)/, "os")
+  inflect.plural(/(z|[u])(?=[A-Z]|_|$)/, "us")
+  inflect.plural(/(z|[d])(?=[A-Z]|_|$)/, "des")
+  inflect.plural(/(z|[j])(?=[A-Z]|_|$)/, "jes")
+  inflect.plural(/(z|[l])(?=[A-Z]|_|$)/, "les")
+  inflect.plural(/(z|[n])(?=[A-Z]|_|$)/, "nes")
+  inflect.plural(/(z|[r])(?=[A-Z]|_|$)/, "res")
+  inflect.plural(/(z|[y])(?=[A-Z]|_|$)/, "yes")
+  inflect.plural(/(z|[z])(?=[A-Z]|_|$)/, "ces")
+
+  #-----------------------------------------------------------------------------
+
+  # Para singularizar, generalmente basta con eliminar la 's' del final, pero
+  # palabras como doctores y universidades se convertirían erróneamente como
+  # doctore y universidade.
+  # Podemos agregar una nueva regla, cuando la palabra termina en "es",
+  # eliminar esa sílaba, pero esto tampoco es del todo cierto, ya que funciona
+  # con palabras como "doctores" o "camiones", porque su singular son "doctor"
+  # y "camion", respectivamente. Sin embargo, hay otras palabras que se
+  # convertirían erróneamente, como semestres, por ejemplo, que al eliminar
+  # la sílaba "es" del final, se singularizaría como 'semestr'. Para evitar
+  # eso, se obliga que la palabra que termina en "es" y venga precedida de r,
+  # y, n, d o l, deba también ser precedida por una vocal.
+  # Primero, definimos las palabras que terminen en 'es', precedido de r, y, n,
+  # d, l o j, precedido de una vocal.
+  # Después, definimos las palabras que terminen en vocal seguida de una 's'. En
+  # caso de que esa vocal sea la e, entonces debe ser precedida de alguna
+  # combinación de las letras b, c, d, f, g, p, t seguida de las letras l o r.
+  # Nota: Por una regla que aplica en Inglés, las palabras terminadas en 'ia'
+  # son plurales de palabras terminadas en 'um', ejemplo 'medium' => 'media'.
+  # Como es Español esa regla no aplica, es necesario especificar que las
+  # palabras terminadas en ia ya están en singular.
+
+  inflect.singular(/((?<singular>ia)|(?<singular>[aeiou][rldyjn])es|(?<singular>[bcdfgpt][lr]e|[aeiou])s)(?=[A-Z]|_|$)/, '\\k<singular>')
+
+  # Para singularizar palabras con 'ces', como 'maices'
+  inflect.singular(/ces$/, 'z')
+
+  #-----------------------------------------------------------------------------
+
+  # Las palabras en singular que terminan con s son un caso especial, ya que
+  # rails infiere que ya están en plural, lo cual es falso. Por ejemplo, pais.
+  # Es necesario indicar que el plural de pais es paises y viceversa. Esto se
+  # puede hacer declarándolo como irregular:
+  inflect.irregular 'pais', 'paises'
+  # Pero es necesario agregar otra regla. Pais termina en s, pero ya está en
+  # singular, por lo que hay que indicarle a rails que no elimine la s final.
+  inflect.singular /(pais)(?=[A-Z]|_|$)/, '\1'
+
+  # el mismo caso aplica para 'mes'
+  inflect.irregular 'mes', 'meses'
+  inflect.singular /(mes)(?=[A-Z]|_|$)/, '\1'
+
+  # Ejemplos cuyo plural y singular sea el mismo
+  inflect.uncountable %w( campus lunes martes miercoles jueves viernes )
+
+  #-----------------------------------------------------------------------------
+
+  # Otros ejemplos de palabras irregulares
+  # En ocasiones, quisiéramos aplicar reglas específicas que no cumplen con las
+  # generales. Por ejemplo, si a estas alturas tratamos de obtener el plural de
+  # "hermano_de_sangre", el resultado sería "hermanos_des_sangres", así que,
+  # para estos casos, es necesario agregar reglas para indicar estos
+  # comportamientos 'irregulares'.
+
+  #inflect.irregular 'hermano_de_sangre', 'hermanos_de_sangre'
+  #inflect.irregular 'HermanoDeSangre', 'HermanosDeSangre'
+
+  # NOTA: es necesario indicar los dos modos (underscore y camelcase) porque el
+  # método irregular no soporta expresiones regulares
+
+  # Caso especial en el que la última palabra termina con 's' desde su versión
+  # en singular (caso similar al de pais, pero en esta ocasión, con palabras
+  # compuestas)
+
+  #inflect.singular 'calificacion_matematicas', 'calificacion_matematicas'
+  #inflect.singular 'CalificacionMatematicas', 'CalificacionMatematicas'
+  #inflect.irregular 'calificacion_matematicas', 'calificaciones_matematicas'
+  #inflect.irregular 'CalificacionMatematicas', 'CalificacionesMatematicas'
+
+  # NOTA: por claridad, se indican los dos modos (underscore y camelcase)
+  # porque el método irregular no soporta expresiones regulares, aunque en el
+  # caso del singular sí, por lo que se puede escribir también de la siguiente
+  # manera:
+  #inflect.singular /([Cc])(alificacion)(_m|M)(atematicas)/, '\1\2\3\4'
+
+end

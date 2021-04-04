a5588bf7044d3f60b6398b1884cae927e491e090
Juan Vasquez Gonzalez <juan@logicalbricks.com>
2021-04-04 00:53:44 -0600

    agrega validaciones de username en usuario

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # RODAL
 
+Inventario y venta de mofles.
+
+## Descripcion
+
+- Productos
+
+  - type
+  - nombre
+  - descripcion
+  - precio de compra
+  - iva, default: 16
+  - precio de venta sugerido
+    - mayoreo
+    - sin instalar
+    - con instalacion
+
+- Productos
+  - Mofles
+  - Aros
+  - Juntas
+
 ## Testing
 
 ```bash
--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -1,4 +1,30 @@
 require "rails_helper"
 
 RSpec.describe Usuario, type: :model do
+  context "validaciones" do
+    it "es valido" do
+      usuario = build :usuario
+
+      expect(usuario).to be_valid
+    end
+
+    it "es invalido" do
+      usuario = build :usuario, username: nil
+
+      expect(usuario).to be_invalid
+      expect(usuario.errors.size).to eq 1
+      expect(usuario.errors[:username][0]).to eq "no puede estar en blanco"
+    end
+
+    context "cuando el username fue tomado" do
+      it "regresa un mensaje de error" do
+        create :usuario, username: "Mateo"
+
+        usuario = build :usuario, username: "mateo"
+        expect(usuario).to be_invalid
+        expect(usuario.errors.size).to eq 1
+        expect(usuario.errors[:username][0]).to eq "ya ha sido tomado"
+      end
+    end # context cuando el username fue tomado
+  end # context validaciones
 end
